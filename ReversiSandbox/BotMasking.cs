ab764131888ba1bbda0796cbe94645340d6a1af6
Fabian van Oeffelt <Fabian.vOeffelt@Gmail.com>
2024-12-20 07:51:34 +0100

    Add Masking Bot

---
+++ ReversiSandbox/BotMasking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversiSandbox
+{
+    public class BotMasking : Bot 
+    {
+        // Value Moves based on a weighted mask multiplied by the amount of tiles flipped
+
+        float[,] weights =
+            {   {16f,8f,8f,8f,8f,8f,8f,16f},
+                {8f,1f,1f,1f,1f,1f,1f,8f},
+                {8f,1f,4f,4f,4f,4f,1f,8f},
+                {8f,1f,4f,1f,1f,4f,1f,8f},
+                {8f,1f,4f,1f,1f,4f,1f,8f},
+                {8f,1f,4f,4f,4f,4f,1f,8f},
+                {8f,1f,1f,1f,1f,1f,1f,8f},
+                {16f,8f,8f,8f,8f,8f,8f,16f}};
+
+        public override Position generateMove(ReversiGame game)
+        {
+            var moves = game.getPossibleMoves();
+            //Dictionary<Position, int> scoreList = new Dictionary<Position, int>();
+            Position bestMove = moves[0];
+            float bestScore = 0;
+            foreach (var move in moves)
+            {
+                float score = weights[move.x, move.y] * ReversiGame.getTilesCaptured(game.gameField, move, game.curPlayer);
+                if(score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = move;
+                }
+            }
+
+            return bestMove;
+        }
+    }
+}
